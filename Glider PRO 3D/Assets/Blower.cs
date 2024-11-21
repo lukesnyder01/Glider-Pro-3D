6e73c0d96ae18ece6ac5590ee8a74b5fba2af87a
lukesnyder01 <119556840+lukesnyder01@users.noreply.github.com>
2024-11-21 08:55:32 -0800

    feat: add dynamic target blower direction updating

--- Glider PRO 3D/Assets/Blower.cs
+++ Glider PRO 3D/Assets/Blower.cs
@@ -6,14 +6,23 @@ public class Blower : MonoBehaviour
 {
     public Vector3 blowerDirection;
 
+    public bool targetBlower;
+    public Transform targetTransform;
+
     void Start()
     {
         blowerDirection = transform.forward;
     }
 
-
-    void Update()
+    void OnTriggerStay(Collider other)
     {
-        
+        if (targetBlower)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                var targetDir = targetTransform.position - other.transform.position;
+                blowerDirection = targetDir.normalized;
+            }
+        }
     }
 }
